e823096db8f779d0b42fbe01d7326933a39d74d3
Aditya Kumar sah <aditya.kumar.sah134@gmail.com>
2026-06-06 12:50:35 +0530

    Add more questions to the quiz

--- OnlineQuizApp.java
+++ OnlineQuizApp.java
@@ -31,11 +31,19 @@ class QuizManager {
     void quick() {
         Scanner sc = new Scanner(System.in);
 
-        Question[] arr = {
-            new Question("Q1: What is Java?", "a) Programming Language", "b) Indian Language", "c) Game", "d) Application", 'a'),
-            new Question("Q2: Who developed Java?", "a) Microsoft", "b) Sun Microsystems", "c) Google", "d) Apple", 'b'),
-            new Question("Q3: What is OOP?", "a) Object Oriented Programming", "b) Open Operating Platform", "c) Output Operation Process", "d) None", 'a')
-        };
+      Question[] arr = {
+    new Question("Q1: What is Java?", "a) Programming Language", "b) Indian Language", "c) Game", "d) Application", 'a'),
+    new Question("Q2: Who developed Java?", "a) Microsoft", "b) Sun Microsystems", "c) Google", "d) Apple", 'b'),
+    new Question("Q3: What is OOP?", "a) Object Oriented Programming", "b) Open Operating Platform", "c) Output Operation Process", "d) None", 'a'),
+    new Question("Q4: Which keyword is used to inherit a class in Java?", "a) extends", "b) implements", "c) inherit", "d) super", 'a'),
+    new Question("Q5: Which of these is not a Java primitive type?", "a) int", "b) float", "c) string", "d) char", 'c'),
+    new Question("Q6: Which method is the entry point of a Java program?", "a) start()", "b) run()", "c) main()", "d) init()", 'c'),
+    new Question("Q7: Which package is automatically imported in Java?", "a) java.util", "b) java.lang", "c) java.io", "d) java.sql", 'b'),
+    new Question("Q8: Which of these is used to handle exceptions?", "a) try-catch", "b) if-else", "c) switch", "d) loop", 'a'),
+    new Question("Q9: Which keyword is used to create an object?", "a) create", "b) new", "c) object", "d) alloc", 'b'),
+    new Question("Q10: Which concept allows multiple methods with the same name but different parameters?", "a) Inheritance", "b) Polymorphism", "c) Overloading", "d) Overriding", 'c')
+};
+
 
         int score = 0;
 
